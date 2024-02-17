16ff291e1e63744cd039da099958ddf13ef9a799
3oheil <143543953+3oheil@users.noreply.github.com>
2024-02-17 03:15:07 -0800

    test in account app

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,11 @@
-from django.test import TestCase, SimpleTestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
+
 
 # Create your tests here.
 
-
+# class AccountPageTest(SimpleTestCase):
+#
+#     def test_url_home_name_exist(self):
+#         response = self.client.get(reverse('register_api'))
+#         self.assertEqual(response.status_code, 200)
